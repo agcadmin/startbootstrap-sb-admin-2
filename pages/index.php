35bfad7f2d76cca1fd7430f5a0b47ce8c0b2f492
Isidro Hernandez Jr <Isidro.hernandez@sap.com>
2018-09-22 18:40:39 -0700

    msg

--- pages/index.php
+++ pages/index.php
@@ -389,7 +389,7 @@
                                 </div>
                                 <div class="col-xs-9 text-right">
                                     <div class="huge">26</div>
-                                    <?php echo "<div> $arrayofDates </div>" ?>
+                                     <div><?php echo $arrayofDates ?></div>" ?>
                                 </div>
                             </div>
 <?php
